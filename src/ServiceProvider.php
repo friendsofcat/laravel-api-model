bfdb483d06bea4f11460d9c85d2570b078206885
David Matta <matta.davi@gmail.com>
2022-08-03 23:47:30 -0500

    Support external scopes

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace MattaDavi\LaravelApiModel;
 
+use Illuminate\Database\Query\Builder;
 use Illuminate\Database\Connection as ConnectionBase;
 use Illuminate\Support\ServiceProvider as ServiceProviderBase;
 
@@ -16,5 +17,30 @@ class ServiceProvider extends ServiceProviderBase
 
             return new Connection($connection, $database, $prefix, $config);
         });
+
+        /*
+         * Adds ability to use external scopes defined within model of API provider.
+         */
+        Builder::macro(
+            'externalScope',
+            function (array|string $scope, mixed $scopeArgs = [])
+            {
+                $this->operators[] = 'scope';
+
+                if (is_array($scope)) {
+                    foreach ($scope as $key => $value) {
+                        if (is_numeric($key)){
+                            $this->where($value, 'scope', '');
+                        } else {
+                            $this->where($key, 'scope', $value);
+                        }
+                    }
+                } else {
+                    $this->where($scope, 'scope', $scopeArgs);
+                }
+
+                array_pop($this->operators);
+            }
+        );
     }
 }
